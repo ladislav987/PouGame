ea0062ed2b7756f10d5767946a06048bb29ef28d
Ladislav <ladislav.chvastas@student.tuke.sk>
2024-12-23 16:31:12 +0100

    fixed collision

--- js/rollingGame/rollingGameCollisionCheck.js
+++
@@ -1,6 +0,0 @@
-// rollingGameCollisionCheck.js
-export function isCollidingWithBoundary(object, boundary) {
-    const objectBox = new THREE.Box3().setFromObject(object);
-    const boundaryBox = new THREE.Box3().setFromObject(boundary);
-    return objectBox.intersectsBox(boundaryBox);
-}
