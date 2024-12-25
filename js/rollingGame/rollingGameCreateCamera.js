cce126a1a815f2bf66c0babd77f848d94fc8ba43
Ladislav <ladislav.chvastas@student.tuke.sk>
2024-12-25 13:23:15 +0100

    added name

--- js/rollingGame/rollingGameCreateCamera.js
+++ js/rollingGame/rollingGameCreateCamera.js
@@ -1,3 +1,4 @@
+// rollingGameCreateCamera.js
 export function createCamera() {
     const camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.01, 1000);
     camera.position.set(0, 10, 150);
